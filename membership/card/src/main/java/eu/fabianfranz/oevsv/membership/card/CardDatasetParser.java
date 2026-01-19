4123f063756169af4e585db19124f415ba7102da
Fabian Franz <franz.fabian.94@gmail.com>
2026-01-19 15:26:06 +0100

    init commit

--- membership/card/src/main/java/eu/fabianfranz/oevsv/membership/card/CardDatasetParser.java
+++ membership/card/src/main/java/eu/fabianfranz/oevsv/membership/card/CardDatasetParser.java
@@ -4,11 +4,23 @@ import java.net.URI;
 import java.util.Optional;
 import java.util.regex.Pattern;
 
+/**
+ * Parser for the Membership card QR-Code.
+ */
 public class CardDatasetParser {
     // I don't get it why there are "l" characters in it when I look at it in the IDE for splitting,
     // shouldn't that be something like pipes
     private final Pattern pattern = Pattern.compile("(\\S+), (.*?) l Mitgliedsnummer: (\\d+) l ADL (\\d+) l (.*)");
+
+    /**
+     * Parse the card data.
+     * @param data the string from the QR code
+     * @return the parsed data or an empty optional if the data is not parsable.
+     */
     public Optional<CardParsingResult> parse(String data) {
+        if (data == null) {
+            return Optional.empty();
+        }
         var matcher = pattern.matcher(data);
         if (!matcher.find()) {
             return Optional.empty(); // string does not match
